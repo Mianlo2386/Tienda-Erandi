45f7be031a6b136cc342e1ce97a33140886e5823
Miguel Lopez <miguellopez32@gmail.com>
2024-07-09 12:07:07 -0300

    Añadido validacion para doble compra y ver el carrito ddesde el header

--- src/main/java/com/tienda/erandibordados/ecommerce/controller/HomeController.java
+++ src/main/java/com/tienda/erandibordados/ecommerce/controller/HomeController.java
@@ -59,7 +59,14 @@ public class HomeController {
         detalleOrden.setTotal(producto.getPrecio()*cantidad);
         detalleOrden.setProducto(producto);
 
-        detalles.add(detalleOrden);
+        //validar para que el producto no se añada dos veces
+        Long idProducto = producto.getId();
+        boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId() == idProducto);
+
+        if (!ingresado){
+            detalles.add(detalleOrden);
+        }
+
 
         sumaTotal = detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
 
@@ -93,6 +100,14 @@ public class HomeController {
         model.addAttribute("orden", orden);
 
 
+
+        return "usuario/carrito";
+    }
+    @GetMapping("/getCart")
+    public String getCart(Model model){
+
+        model.addAttribute("cart", detalles);
+        model.addAttribute("orden", orden);
 
         return "usuario/carrito";
     }
