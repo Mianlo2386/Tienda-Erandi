d6a5b18636bdb90938128201b152702f48e759de
Miguel Lopez <miguellopez32@gmail.com>
2024-07-14 21:25:53 -0300

    Listo para verificar error detalle orden

--- src/main/java/com/tienda/erandibordados/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/tienda/erandibordados/ecommerce/controller/AdministradorController.java
@@ -4,10 +4,13 @@ import com.tienda.erandibordados.ecommerce.model.Producto;
 import com.tienda.erandibordados.ecommerce.service.IOrdenService;
 import com.tienda.erandibordados.ecommerce.service.IUsuarioService;
 import com.tienda.erandibordados.ecommerce.service.ProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,6 +26,7 @@ public class AdministradorController {
 
     @Autowired
     private IOrdenService ordenService;
+    private Logger logg = LoggerFactory.getLogger(AdministradorController.class);
 
     @GetMapping("")
     public String home(Model model){
@@ -39,5 +43,10 @@ public class AdministradorController {
     public String ordenes(Model model){
         model.addAttribute("ordenes", ordenService.findAll());
         return "administrador/ordenes";
+    }
+    @GetMapping("/detalle/{id}")
+    public String detalle(Model model, @PathVariable Long id){
+        logg.info("Id de la orden: {}",id );
+        return "administrador/detalleorden";
     }
 }
